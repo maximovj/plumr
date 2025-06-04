a3d296394f75836d7db16a7865c83b9bf4e3ab16
Víctor J <maximovj@outlook.com>
2025-06-04 10:57:39 -0600

    feature: Se instala livewire

---
+++ resources/views/livewire/welcome-card.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <h3>{{ $titulo }}</h3>
+    <h4>{{ $subtitulo }}</h4>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non cumque facilis laudantium explicabo sed. Mollitia quam commodi voluptates doloremque aut neque veritatis accusamus. Adipisci omnis quis, expedita libero ipsam illo!</p>
+</div>
