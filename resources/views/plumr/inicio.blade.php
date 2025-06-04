7c3a1f19289cb2bbb35006861528129ffefcbd29
Víctor J <maximovj@outlook.com>
2025-06-04 10:04:32 -0600

    feature: Se instala alpinejs

--- resources/views/plumr/inicio.blade.php
+++ resources/views/plumr/inicio.blade.php
@@ -2,4 +2,15 @@
 
 @section('main')
 <h1>Página principal</h1>
+
+<hr>
+
+<div x-data="{ open: false }">
+    <button @click="open = !open">Mostrar mensaje</button>
+
+    <p x-show="open" class="mt-2 text-green-500">
+        ¡Hola desde Alpine.js! 🎉
+    </p>
+</div>
+
 @endsection
