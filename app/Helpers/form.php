53afec343b93cdcae8d0ea61298de9c773357a47
Víctor J <maximovj@outlook.com>
2025-06-09 12:23:47 -0600

    feature: Se modifica el helper

--- app/Helpers/form.php
+++ app/Helpers/form.php
@@ -8,9 +8,12 @@ if(!function_exists('e_class'))
 {
     function e_class($field)
     {
+        if(!session('errors'))
+        return 'border-1 border-gray-400';
+
         if(session('errors') && session('errors')->has($field) )
-            return 'border-2 border-red-600 hover:border-red-600 focus:border-red-600 active:border-red-600';
+        return 'border-2 border-red-600 hover:border-red-600 focus:border-red-600 active:border-red-600';
         else
-            return 'border-1 border-gray-400';
+        return 'border-2 border-green-400 hover:border-green-400 focus:border-green-400 active:border-green-400';
     }
 }
