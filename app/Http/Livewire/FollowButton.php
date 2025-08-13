2090228f09ab0ed9a275607c971693010a9c316f
Víctor J <maximovj@outlook.com>
2025-08-13 10:08:46 -0600

    feature: Se modifica el botón de seguidores

--- app/Http/Livewire/FollowButton.php
+++ app/Http/Livewire/FollowButton.php
@@ -8,20 +8,24 @@ use Livewire\Component;
 class FollowButton extends Component
 {
     public User $user;
+    public $isFollowing;
 
     public function mount($user)
     {
         $this->user = $user;
+        $this->isFollowing = auth()->user()->followings->contains($user->id);
     }
 
     public function toggleFollow()
     {
         $authUser = auth()->user();
 
-        if ($authUser->followings->contains($this->user->id)) {
+        if ($this->isFollowing) {
             $authUser->followings()->detach($this->user->id);
+            $this->isFollowing = false;
         } else {
             $authUser->followings()->attach($this->user->id);
+            $this->isFollowing = true;
         }
     }
 
