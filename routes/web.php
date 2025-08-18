3afc6a8a23c019abb1ddf9cc21e187e4b8ce6f74
Víctor J <maximovj@outlook.com>
2025-08-18 11:38:56 -0600

    feature: Se agrega HomeController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\MainAccountController;
@@ -19,9 +20,7 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('plumr.inicio');
-});
+Route::get('/', HomeController::class)->name('home');
 
 // Rutas para login
 Route::get('/login', [LoginController::class, 'index'])->name('login');
