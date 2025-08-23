e6360cc96cdbba9b1f94cb59b2a6ffd4e10ac3cc
Víctor J <maximovj@outlook.com>
2025-08-23 03:53:30 -0600

    feature: Se instala Laravel FilePond Backend

--- resources/views/plumr/layout/app.blade.php
+++ resources/views/plumr/layout/app.blade.php
@@ -3,6 +3,13 @@
 <x-head></x-head>
 <style>[x-cloak]{ display:none !important; }</style>
 <body class="bg-gray-50 min-h-screen flex flex-col">
+    @livewireScripts
+    {{-- Quill is a modern rich text editor built for compatibility and extensibility.  --}}
+    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/scrapooo/quill-resize-module@1.0.2/dist/quill-resize-module.js"></script>
+
+    <script src="{{ asset('js/filepond.js') }}"></script>
+
     @livewire('confirm-delete-model-class')
     @livewire('post-modal-form')
 
@@ -10,12 +17,6 @@
     <x-app-error-message></x-app-error-message>
     @yield('main')
     <x-footer></x-footer>
-
-    {{-- Quill is a modern rich text editor built for compatibility and extensibility.  --}}
-    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/scrapooo/quill-resize-module@1.0.2/dist/quill-resize-module.js"></script>
-
-    @livewireScripts
 
 </body>
 </html>
