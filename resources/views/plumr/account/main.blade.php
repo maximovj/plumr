d783fa11da17ffea529c9c465021452e7b59f9d1
Víctor J <maximovj@outlook.com>
2025-08-25 12:47:45 -0600

    feature: Se modifica vista de cuenta de usuario

--- resources/views/plumr/account/main.blade.php
+++ resources/views/plumr/account/main.blade.php
@@ -11,10 +11,22 @@
             <div class="relative h-40">
                 <div class="h-full w-full bg-center bg-cover" style="background-image: url('{{ $user->profile->cover_url }}')"></div>
 
-                <!-- Foto de usuario -->
                 <div class="absolute -bottom-12 left-6">
+                    <!-- Foto de usuario -->
                     <img src="{{ $user->profile->photo_url }}" alt="Foto de usuario"
-                        class="w-28 h-28 rounded-full border-4 border-white shadow-lg cursor-pointer hover:scale-105 transition transform">
+                    class="w-28 h-28 rounded-full border-4 border-white shadow-lg cursor-pointer hover:scale-105 transition transform">
+
+                    <!-- Botones de acciones (solo para el dueño) -->
+                    <div class="absolute -bottom-1 -right-10  mt-4 flex justify-between items-center gap-2">
+                        @if(Auth::check() && Auth::user()->id === $user->id)
+                            <a href="{{ route('account.edit', ['user' => $user]) }}"
+                                class="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center text-white shadow hover:bg-blue-600 transition">
+                                <i class="bi bi-gear"></i>
+                            </a>
+                        @endif
+
+                        @livewire('list-followers', ['user' => $user])
+                    </div>
                 </div>
 
                 <!-- Botón de acciones (solo para el dueño) -->
@@ -70,17 +82,7 @@
                     @isset($profile->address)<span><i class="bi bi-house-fill"></i> {{ $profile->address }}</span>@endisset
                 </div>
 
-                <!-- Botones de acciones (solo para el dueño) -->
-                <div class="mt-4 flex justify-between items-center gap-2">
-                    @if(Auth::check() && Auth::user()->id === $user->id)
-                        <a href="{{ route('account.edit', ['user' => $user]) }}"
-                            class="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center text-white shadow hover:bg-blue-600 transition">
-                            <i class="bi bi-gear"></i>
-                        </a>
-                    @endif
 
-                    @livewire('list-followers', ['user' => $user])
-                </div>
             </div>
 
             <hr class="my-2">
