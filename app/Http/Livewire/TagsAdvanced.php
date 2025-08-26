0a5a53964a8540fdfc8beedce6c4bcf5de1d22ce
Víctor J <maximovj@outlook.com>
2025-08-26 04:34:38 -0600

    feature: Se modifica el componente de livewire

--- app/Http/Livewire/TagsAdvanced.php
+++ app/Http/Livewire/TagsAdvanced.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class TagsAdvanced extends Component
 {
+    public User $user;
     public $tags;
 
-    public function mount($tags = [])
+    public function mount(User $user, $tags = [])
     {
+        $this->user = $user;
         $this->tags = collect($tags)->filter(fn($item) => !empty($item));
     }
 
--- resources/views/livewire/tags-advanced.blade.php
+++ resources/views/livewire/tags-advanced.blade.php
@@ -2,7 +2,7 @@
 @if (!$tags->isEmpty())
     <div class="flex gap-2 flex-wrap">
         @foreach ($tags as $tag)
-            @if ($account_tag = is_account_tag($tag))
+            @if ($account_tag = is_account_tag($user, $tag))
                 <a href="{{ route('main_account', [$account_tag]) }}">
                     <span class="bg-indigo-500 hover:bg-indigo-600 border-2 text-white rounded-full px-2 py-1 text-xs">
                         <i class="bi bi-person-fill"></i> {{ $account_tag->username }}
