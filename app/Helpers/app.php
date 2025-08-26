311492f185591311c280e0a89e8df17dec9da3dd
Víctor J <maximovj@outlook.com>
2025-08-26 03:59:28 -0600

    feature: Se agrega etiqueta para cuentas y se crea un helper para el mismo propósito

--- app/Helpers/app.php
+++ app/Helpers/app.php
@@ -9,3 +9,10 @@ if(!function_exists('isfollower')) {
         return auth()->user()->followings->contains($user_check->id);
     }
 }
+
+if(!function_exists('is_account_tag')) {
+    function is_account_tag(string $tag)
+    {
+        return auth()->user()->followings()->where('username', ltrim($tag, '@'))->first();
+    }
+}
--- resources/views/plumr/account/albums/index.blade.php
+++ resources/views/plumr/account/albums/index.blade.php
@@ -115,10 +115,10 @@
                         <div class="flex gap-2 flex-wrap">
                             @foreach ($album->tags as $tag)
 
-                            @if($account_tag = auth()->user()->followings()->where('username', ltrim($tag, '@'))->first())
+                            @if($account_tag = is_account_tag($tag))
                             <a href="{{ route('main_account', [$account_tag]) }}">
-                                <span class="bg-indigo-400 border-2 text-white rounded-full px-2 py-1 text-xs">
-                                    <i class="bi bi-tag-fill"></i> {{ $tag }}
+                                <span class="bg-indigo-500 hover:bg-indigo-600 border-2 text-white rounded-full px-2 py-1 text-xs">
+                                    <i class="bi bi-person-fill"></i> {{ $account_tag->username }}
                                 </span>
                             </a>
                             @else
