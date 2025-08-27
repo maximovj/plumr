c9c3aa299c561bff0b25907c67837e4b0b746f9a
Víctor J <maximovj@outlook.com>
2025-08-27 11:05:25 -0600

    feature: Se crea componente livewire para mover una Media a otros álbumes del usuario

---
+++ app/Http/Livewire/MediaMoveToAlbum.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Album;
+use App\Models\Media;
+use App\Models\User;
+use Livewire\Component;
+
+class MediaMoveToAlbum extends Component
+{
+    // Variables
+    public Media $media;
+    public $showModal = false;
+    public array $albums_selected = [];
+    public array $albums = [];
+
+    // Props
+    public $redirect = null;
+    public $albumId;
+    public $mediaId;
+    public $userId;
+
+    protected $listeners = [
+        'mediaMoveToAlbum' => 'openModal',
+        'updateAlbumsSelected',
+    ];
+
+    public function mount()
+    {
+        $this->showModal = false;
+        $this->albums_selected = []; // 👈 asegura array vacío
+        $this->albums = []; // 👈 asegura array vacío
+    }
+
+    public function openModal($albumId, $mediaId, $userId, $redirect = null)
+    {
+        $this->albumId = $albumId;
+        $this->mediaId = $mediaId;
+        $this->userId = $userId;
+        $this->redirect = $redirect;
+
+        $user = User::find($this->userId);
+        $this->albums = $user->albums()->orderBy('title')->get()->toArray() ?? [];
+        $this->albums_selected[]  = $this->albumId;
+
+        $this->showModal = true;
+    }
+
+    public function updateAlbumsSelected($value)
+    {
+        $this->albums_selected = $value;
+    }
+
+    public function moveToAlbum()
+    {
+
+        $this->showModal = false;
+
+        $media = Media::find($this->mediaId);
+        $album = Album::find($this->albumId);
+
+
+        if(empty($this->albums_selected)) {
+            dd('En ningún álbum');
+        }else
+        if($media && $album && $user = $album->user) {
+            if(isowner($user)) {
+                $media->albums()->sync($this->albums_selected);
+                toastr()->addSuccess('Multimedia modificado correctamente');
+
+                sweetalert()
+                ->showConfirmButton(
+                true,
+                "Enterado",
+                "btn btn-success",
+                "Enterado"
+                )->addSuccess('Multimedia modificado correctamente');
+            }
+        }
+
+        if ($this->redirect) {
+            return redirect()->to($this->redirect);
+        }
+
+        // Emitimos un evento global
+        $this->emit('updatedAlbum', $this->albumId);
+    }
+
+    public function render()
+    {
+        return view('livewire.media-move-to-album');
+    }
+}
---
+++ resources/views/livewire/media-move-to-album.blade.php
@@ -0,0 +1,77 @@
+<div x-data="{
+        open: @entangle('showModal'),
+        closing: false,
+        close(type) {
+            this.closing = true;
+
+            // Animaciones opcionales (solo para visual)
+            this.$refs.overlay.classList.remove('animate__fadeIn');
+            this.$refs.modal.classList.remove('animate__zoomIn','animate__bounceIn','animate__slideInDown');
+
+            if(type === 'cancel') {
+                this.$refs.modal.classList.add('animate__slideOutDown');
+            } else if(type === 'confirm') {
+                this.$refs.modal.classList.add('animate__zoomOut','animate__fadeOut');
+            }
+
+            // Cerrar inmediatamente
+            this.open = false;
+            this.closing = false;
+
+            this.$refs.modal.classList.remove('animate__zoomOut','animate__fadeOut','animate__slideOutDown');
+            this.$refs.overlay.classList.remove('animate__fadeOut');
+        }
+    }"
+    x-init="
+    $watch('open', value => @this.set('showModal', value));
+    "
+    x-cloak
+>
+    <!-- Overlay -->
+    <div x-show="open"
+         x-ref="overlay"
+         class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 animate__animated animate__faster animate__fadeIn">
+
+        <!-- Modal -->
+        <div x-ref="modal"
+            class="
+            flex flex-col justify-between items-stretch
+            bg-white rounded-lg shadow-lg p-6 w-96 animate__animated animate__faster animate__zoomIn"
+            style="height: 70vh !important;">
+
+            <!-- -->
+            <section>
+                <h4 class="font-semibold py-2">Lista de álbum</h4>
+                <div class="w-full h-96 overflow-auto relative">
+                    <ul class="space-y-4">
+                        <template x-for="album in $wire.albums" :key="album['id']">
+                            <li class="flex justify-start items-center gap-4 w-full">
+                                <input type="checkbox" name="album[]" :value="album['id']"
+                                x-model="$wire.albums_selected"
+                                class="w-6 h-6 inline">
+                                <span x-text="album['title']" class="block"></span>
+                            </li>
+                        </template>
+                    </ul>
+
+                </div>
+            </section>
+
+            <div class="mt-6 flex justify-end space-x-2">
+                <button @click="close('cancel')"
+                        class="px-4 py-2 bg-gray-300 rounded">
+                    Cancelar
+                </button>
+                <button
+                    wire:click="moveToAlbum"
+                    x-on:click="
+                        //$wire.moveToAlbum(albums_selected);
+                        close('confirm');
+                    "
+                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded">
+                    Confirmar
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
